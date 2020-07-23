de5dd488e541806aeb9a71e053a0ab63819f90bf
Ivan Kostiv <ivankostiv.pz@gmail.com>
2020-07-23 13:01:18 +0300

    Create project skeleton

---
+++ movie_shooter.client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Routers from './routers';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Routers />
+    </div>
+  );
+}
+
+export default App;

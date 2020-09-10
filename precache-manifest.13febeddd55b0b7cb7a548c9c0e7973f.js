4afbd7c75b7738537e9287f517cc981317a2b7bb
Ivan Kostiv <ivankostiv.pz@gmail.com>
2020-09-10 21:28:00 +0300

    Updates

---
+++ precache-manifest.13febeddd55b0b7cb7a548c9c0e7973f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c69f2750e31cb7b9764cde94827fc8e",
+    "url": "/IvanKostiv.github.io/movie_shooter.client/index.html"
+  },
+  {
+    "revision": "dac4942d7bc58bfe7ec0",
+    "url": "/IvanKostiv.github.io/movie_shooter.client/static/css/main.6fbcb00e.chunk.css"
+  },
+  {
+    "revision": "24749c0fbf89a2058a4d",
+    "url": "/IvanKostiv.github.io/movie_shooter.client/static/js/2.c72480d8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/IvanKostiv.github.io/movie_shooter.client/static/js/2.c72480d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dac4942d7bc58bfe7ec0",
+    "url": "/IvanKostiv.github.io/movie_shooter.client/static/js/main.ac7babde.chunk.js"
+  },
+  {
+    "revision": "b3fb1188e7a99eb93adb",
+    "url": "/IvanKostiv.github.io/movie_shooter.client/static/js/runtime-main.fc1f3e10.js"
+  }
+]);
